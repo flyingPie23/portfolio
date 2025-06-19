1ca7e786bcc38295d153a070b2e1f9c02c10d320
driss <freah>
2025-06-19 19:27:55 +0100

    workingon the main page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,22 @@
 
 function Tabs(props) {
+  const handleClick = () => {
+    const section = document.getElementById(props.tab);
+    console.log(section)
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <button className="text-white hover:text-light-blue focus:text-light-blue focus:underline underline-offset-[4px] "><strong className="text-light-blue mr-[12px]">{props.order}</strong>{props.tab}</button>
-  )
+    <button
+      onClick={handleClick}
+      className="text-white hover:text-light-blue focus:text-light-blue focus:underline underline-offset-[4px]"
+    >
+      <strong className="text-light-blue mr-[12px]">{props.order}</strong>
+      {props.tab}
+    </button>
+  );
 }
 
 function Icon(props) {
@@ -10,28 +24,25 @@ function Icon(props) {
     <button className="material-symbols-outlined text-white font-bold text-[50px] hover:text-light-blue">
       code
     </button>
-  )
+  );
 }
 
 function Navbar(props) {
-
   return (
     <header>
       <nav className="bg-dark-blue p-[24px] shadow-md grid gap-4 grid-cols-3 grid-rows-1 items-center justify-items-center font-header ">
         <div className="grid gap-[32px] grid-cols-2 grid-rows-1 items-center justify-items-center">
-          <Tabs order="1." tab="About"/>
-          <Tabs order="2." tab="Experience"/>
+          <Tabs order="1." tab="About" aboutRef={props.aboutRef} />
+          <Tabs order="2." tab="Experience" />
         </div>
-
         <Icon />
-
         <div className="grid gap-[32px] grid-cols-2 grid-rows-1 items-center justify-items-center">
-          <Tabs order="3." tab="Work"/>
-          <Tabs order="4." tab="Contact"/>
+          <Tabs order="3." tab="Work" />
+          <Tabs order="4." tab="Contact" />
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
