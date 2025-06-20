e38123ef642588a62aecd9add950419f82cf2201
driss <freah>
2025-06-20 17:25:54 +0100

    workingon section 5

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,9 +3,9 @@ import Navbar from "../components/navbar";
 import { useState } from 'react';
 
 
-function Section1() {
+function Section1(props) {
   return (
-    <section className="md:my-[200px] md:mx-[150px] lg:my-[250px] lg:mx-[200px]">
+    <section className="md:mt-[150px] md:mb-[250px] md:mx-[150px] md:py-[40px] lg:mt-[250px] lg:mb-[300px] lg:mx-[200px] lg:py-[80px]">
       <h2 className="font-header text-[28px] text-light-blue">Hello! welcome to my portfolio.</h2>
       <h1 className="font-header text-[48px] font-extrabold text-white">Driss Freah</h1>
       <p className="text-light-gray">
@@ -19,7 +19,7 @@ function Section1() {
 
 function Section2(props) {
   return (
-  <section id="About" className="md:my-[150px] md:mx-[150px] md:py-[40px] lg:my-[200px] lg:mx-[200px]">
+  <section id="About" className="md:mt-[150px] md:mb-[250px] md:mx-[150px] md:py-[40px] lg:mt-[250px] lg:mb-[300px] lg:mx-[200px] lg:py-[80px]">
     <h1 className="before:content-['1.'] before:mr-[10px] before:text-light-blue before:font-bold  text-white font-header text-[28px]">Who i am:</h1>
     <hr className="border-t border-light-gray my-4" />
     <div className="flex">
@@ -106,7 +106,7 @@ function Section3(props) {
   }
 
   return (
-    <section id="Experience" className="md:my-[150px] md:mx-[150px] md:py-[40px] lg:my-[200px] lg:mx-[200px]">
+    <section id="Experience" className="md:mt-[150px] md:mb-[250px] md:mx-[150px] md:py-[40px] lg:mt-[250px] lg:mb-[300px] lg:mx-[200px] lg:py-[80px]">
        <h1 className="before:content-['2.'] before:mr-[10px] before:text-light-blue before:font-bold  text-white font-header text-[28px]">My Journey:</h1>
        <hr className="border-t border-light-gray my-4" />
 
@@ -132,23 +132,57 @@ function Section3(props) {
 function Section4(props) {
   const WorkCard = (props)=> {
     return (
-     <div className="text-white h-[250px] my-[22px] p-[24px] border border-blue ease-in duration-300 hover:bg-blue hover:translate-y-[-10px]">
-      <div>
+      <div className="text-white h-[300px] my-[10px] p-[34px] border border-blue ease-in duration-300 hover:bg-blue hover:translate-y-[-12px] group relative">
+        <div className="flex justify-center justify-between mb-[24px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            className="text-blue group-hover:text-white transition-colors duration-300"
+          >
+            <path
+              d="M10.414 4l2 2H20c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2h6.414zM4 6v12h16V8h-8.414l-2-2H4z"
+              fill="currentColor"
+            />
+          </svg>
 
+          <a href={props.project.link} className="hover:translate-y-[-8px] ease-in duration-300">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className="inline-block ml-2 align-middle text-blue group-hover:text-white transition-colors duration-300">
+              <path d="M14 3v2h3.59L10 12.59 11.41 14 19 6.41V10h2V3h-7zm-9 4h7V5H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7H5V7z" fill="currentColor"/>
+            </svg>
+          </a>
+        </div>
 
+        <h1 className="font-header font-bold text-light-blue group-hover:text-white transition-colors duration-300 text-[28px]">
+          {props.project.title}
+        </h1>
+
+        <p className="text-light-gray font-header font-semibold max-w-[70%] group-hover:text-white transition-colors duration-300">{props.project.description}.</p>
+
+        <div className="flex flex-row content-end absolute bottom-[34px] left-[34px]  text-light-gray">
+          {
+            props.project.tools && props.project.tools.map((tool, idx) => (
+              <p key={idx} className="mr-[12px] font-header font-semibold max-w-[70%] group-hover:text-white transition-colors duration-300  group-hover:text-white transition-colors duration-300">{tool}</p>
+            ))
+          }
+
+        </div>
       </div>
-     </div>
     )
   }
   return (
-    <section id="Work" className="md:my-[150px] md:mx-[150px] md:py-[40px] lg:my-[200px] lg:mx-[200px]">
+    <section id="Work" className="md:mt-[150px] md:mb-[250px] md:mx-[150px] md:py-[40px] lg:mt-[250px] lg:mb-[300px] lg:mx-[200px] lg:py-[80px]">
       <h1 className="before:content-['3.'] before:mr-[10px] before:text-light-blue before:font-bold  text-white font-header text-[28px]">What i have worked on:</h1>
       <hr className="border-t border-light-gray my-4" />
 
-      <div className="grid grid-cols-3 gap-[24px]">
-        <WorkCard/>
-        <WorkCard/>
-        <WorkCard/>
+      <div className="grid grid-cols-3 gap-[12px]">
+        {
+          props.projects && props.projects.map((project,idx)=> (
+            <WorkCard key={idx} project={project}/>
+          ))
+        }
       </div>
 
     </section>
@@ -157,8 +191,35 @@ function Section4(props) {
 
 }
 
+function Section5(props) {
+  return (
+    <section id="Contact" className="md:mt-[150px] md:mb-[250px] md:mx-[150px] md:py-[40px] lg:mt-[250px] lg:mb-[300px] lg:mx-[200px] lg:py-[80px]">
+      <h1 className="before:content-['4.'] before:mr-[10px] before:text-light-blue before:font-bold  text-white font-header text-[28px]">How to get in touch:</h1>
+      <hr className="border-t border-light-gray my-4" />
+
+      <div className="text-center mx-auto">
+        <h1 className="">Intrested with my work ?</h1>
+        <p>Im always looking for new opertunities</p>
+
+      </div>
+
+    </section>
+  )
+}
+
 function App() {
   const skills = ["Html5","Css","Javascript","Sql","Postgress","Heroku deployment","Ruby on Rails", "React js", "Figma"];
+  const projects = [
+    {link: "https://github.com/flyingPie23", title: "this a title1", description: "a quick description of the project.1", tools: ["tool", "tool"]},
+    {link: "https://github.com/flyingPie23", title: "this a title2", description: "a quick description of the project.2", tools: ["tool", "tool", "tool"]},
+    {link: "https://github.com/flyingPie23", title: "this a title3", description: "a quick description of the project.3", tools: ["tool", "tool", "tool"]},
+    {link: "https://github.com/flyingPie23", title: "this a title4", description: "a quick description of the project.4", tools: ["tool"]},
+    {link: "https://github.com/flyingPie23", title: "this a title5", description: "a quick description of the project.5", tools: ["tool"]},
+    {link: "https://github.com/flyingPie23", title: "this a title6", description: "a quick description of the project.6", tools: ["tool",]},
+    {link: "https://github.com/flyingPie23", title: "this a title7", description: "a quick description of the project.7", tools: ["tool", "tool"]},
+    {link: "https://github.com/flyingPie23", title: "this a title8", description: "a quick description of the project.8", tools: ["tool", "tool"]},
+    {link: "https://github.com/flyingPie23", title: "this a title9", description: "a quick description of the project.9", tools: ["tool"]}
+  ]
 
   return (
     <div className="main-page relative">
@@ -166,7 +227,8 @@ function App() {
       <Section1 />
       <Section2 skills={skills} />
       <Section3/>
-      <Section4/>
+      <Section4 projects={projects}/>
+      <Section5/>
     </div>
   );
 }
